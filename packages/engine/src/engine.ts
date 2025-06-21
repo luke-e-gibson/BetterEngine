5fd2d855e8a0037fa08beae1e8f303aa2e18201d
Luke Gibson <luke.e.gibson@outlook.com>
2025-06-21 23:08:25 +1000

    Revert "Engine types for world, gameobject, components"
    This reverts commit 617574aa45b2c97e8d402a17a233f8b6e032d9bb.

--- packages/engine/src/engine.ts
+++ packages/engine/src/engine.ts
@@ -1,30 +1,10 @@
+
 export class Engine {
-  private config: Internal.EngineConfig;
-  private world: Engine.World.World | null = null;
-
-  constructor(config: Partial<Internal.EngineConfig>) {
-    this.config = {
-      ...Engine.defaultConfig(),
-      ...config,
-    }
-
-
-  }
+  constructor() {}
 
   public async initialize(): Promise<void> {
-    if(this.config.world) {
-      if (typeof this.config.world === "string") {
-        const response = await fetch(this.config.world);
-        if (!response.ok) {
-          throw new Error(`Failed to load world: ${response.statusText}`);
-        }
-        const worldData = await response.json();
-        this.world = worldData as Engine.World.World;
-      } else {
-        // Use the provided world object directly
-        this.world = this.config.world;
-      }
-    }
+    
+    
   }
 
   public start(): void {
@@ -33,16 +13,5 @@ export class Engine {
 
   private update(): void {
 
-  }
-
-  private static defaultConfig(): Internal.EngineConfig {
-    return {
-      flags: {
-        debug: false, // Default to not debugging
-        verbose: false, // Default to not verbose logging
-      },
-      world: "/game/world.json", // Default world name
-      resources: {}, // Empty resources by default
-    }
   }
 }
--- packages/engine/tsup.config.ts
+++ packages/engine/tsup.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/engine.ts"],
   clean: true,
-  format: ["esm"],
+  format: ["cjs", "esm"],
   dts: true,
 });
